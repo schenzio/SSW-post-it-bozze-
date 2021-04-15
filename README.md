33aa4d31ebaf050f99102b3b3ab2fb6ad285111e
schenzio <matteo.scanna1999@gmail.com>
2021-04-15 19:19:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-xur4fh-j4wgn4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-xur4fh-j4wgn4)
---
+++ src/app/app.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'I miei post-it';
+  postits =[
+    {
+      id: 1,
+      testo: "Il mio primo post-it",
+      colore: "black",
+      data: "15/04/2021"
+    },
+     {
+      id: 2,
+      testo: "Il mio secondo post-it",
+      colore: "black",
+      data: "15/04/2021"
+    },
+     {
+      id:3,
+      testo: "Il mio terzo post-it",
+      colore: "black",
+      data: "15/04/2021"
+    }
+  ]
+  cities = [
+    {
+      nome: 'Torino',
+      temperatura: '14'
+    }, {
+      nome: 'Milano',
+      temperatura: '15'
+    }, {
+      nome: 'Genova',
+      temperatura: '18'
+    }
+  ];
+  selezionata: string;
+  seleziona(name: string) {
+    this.selezionata = name;
+  }
+  clean() {
+    this.selezionata=undefined;
+  }
+}
